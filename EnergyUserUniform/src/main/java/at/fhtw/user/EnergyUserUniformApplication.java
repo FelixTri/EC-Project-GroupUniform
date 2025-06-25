15e89d8b6e638691cc22f24b9bc334b0b07da294
FelixTri <felix.trieblnig@gmail.com>
2025-06-26 01:33:27 +0200

    Added User Usage Service

--- EnergyUserUniform/src/main/java/at/fhtw/user/EnergyUserUniformApplication.java
+++ EnergyUserUniform/src/main/java/at/fhtw/user/EnergyUserUniformApplication.java
@@ -2,9 +2,12 @@ package at.fhtw.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EnergyUserUniformApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(EnergyUserUniformApplication.class, args);
     }
--- EnergyUserUniform/src/main/java/at/fhtw/user/model/ProducerMessage.java
+++ EnergyUserUniform/src/main/java/at/fhtw/user/model/ProducerMessage.java
@@ -1,20 +1,71 @@
 package at.fhtw.user.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.LocalDateTime;
 
 public class ProducerMessage {
+
     private String type;
     private String association;
     private double kwh;
     private LocalDateTime datetime;
 
-    public String getType() { return type; }
-    public String getAssociation() { return association; }
-    public double getKwh() { return kwh; }
-    public LocalDateTime getDatetime() { return datetime; }
+    public ProducerMessage() {
+    }
 
-    public void setType(String type) { this.type = type; }
-    public void setAssociation(String association) { this.association = association; }
-    public void setKwh(double kwh) { this.kwh = kwh; }
-    public void setDatetime(LocalDateTime datetime) { this.datetime = datetime; }
+    @JsonCreator
+    public ProducerMessage(
+            @JsonProperty("type") String type,
+            @JsonProperty("association") String association,
+            @JsonProperty("kwh") double kwh,
+            @JsonProperty("datetime") LocalDateTime datetime) {
+        this.type = type;
+        this.association = association;
+        this.kwh = kwh;
+        this.datetime = datetime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getAssociation() {
+        return association;
+    }
+
+    public double getKwh() {
+        return kwh;
+    }
+
+    public LocalDateTime getDatetime() {
+        return datetime;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAssociation(String association) {
+        this.association = association;
+    }
+
+    public void setKwh(double kwh) {
+        this.kwh = kwh;
+    }
+
+    public void setDatetime(LocalDateTime datetime) {
+        this.datetime = datetime;
+    }
+
+    @Override
+    public String toString() {
+        return "ProducerMessage{" +
+                "type='" + type + '\'' +
+                ", association='" + association + '\'' +
+                ", kwh=" + kwh +
+                ", datetime=" + datetime +
+                '}';
+    }
 }
